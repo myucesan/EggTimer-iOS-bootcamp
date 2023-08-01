53cdc761eaf22b07a2ade13ce99d957dd2e2e3c7
Mustafa <Ycesan>
2023-08-01 21:15:23 +0200

    Switched the seconds so it counts up and the progress bar fills correctly. BUG: have to fix when you start a new timer or switch in between, have to make sure that works correctly

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -12,25 +12,28 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mainMessage: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    var eggTimes : [String : Int] = ["Soft" : 1, "Medium" : 7, "Hard" : 12]
+    var eggTimes : [String : Int] = ["Soft" : 1, "Medium" : 1, "Hard" : 1]
     var seconds : Int = 0
     var timer : Timer? = nil // alternatively var timer = Timer() so we don't have to use optionals
     var hardness : String = ""
     
     @IBAction func hardnessPressed(_ sender: UIButton) {
-        progressBar.setProgress(1, animated: true) // progressBar.progress = value should also work, had made a wrong @IBOutlet so it didn't work before and found setProgress because of it. We have animation now at least
+
         timer?.invalidate()
         hardness = sender.currentTitle!
-        seconds = eggTimes[hardness]! * 60
+//        seconds = eggTimes[hardness]! * 60
+        
         print("You're egg is \(hardness) and it takes \(eggTimes[hardness]!) minutes to boil.")
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
     }
 
     @objc func fireTimer() {
         print(seconds)
-        seconds -= 1
+        seconds += 1
         
-        if seconds == 0 {
+        var forProgressBar : Float = Float(seconds) / Float((eggTimes[hardness]! * 60))
+        progressBar.setProgress(forProgressBar, animated: true) // progressBar.progress = value should also work, had made a wrong @IBOutlet so it didn't work before and found setProgress because of it. We have animation now at least
+        if seconds == eggTimes[hardness]! * 60 {
             print("timer ends")
             mainMessage.text = "\(hardness) egg has finished boiling"
             timer?.invalidate()
